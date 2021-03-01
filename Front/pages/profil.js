42e194263f556f221727b1b60f51380cf5204c9b
angeliquesvt <angelique.souvant@gmail.com>
2021-03-01 14:47:49 +0100

    pb profil

--- Front/pages/profil.js
+++ Front/pages/profil.js
@@ -1,29 +1,47 @@
 import Link from "next/link";
 import {Container} from "../node_modules/react-bootstrap";
+import {useEffect, useState} from "react";
 
+export async function getStaticProps(context) {
 
-export async function getUserInformation() {
-    const ISSERVER = typeof window === "undefined";;
-    if(!ISSERVER) {
-        const userId= localStorage.getItem('userId')
-        const res = await fetch(`http://${process.env.PROFIL_HOST}:5006/profil/${userId}`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                method: 'GET'
-            })
-        const result = await res.json();
-        return result.username;
+    return {
+     props: {
+         username:"fail"
+     }
+    };
 
-    }
 }
-function Profil() {
-    getUserInformation();
 
-    return (
+const Profil = (props) => {
+    const [data, dataSet] = useState<any>({
+
+    })
+
+    useEffect(async () => {
+        const ISSERVER = typeof window === "undefined";
+        if(!ISSERVER) {
+            console.log("ALO")
+            const userId= localStorage.getItem('userId')
+            const res = await fetch(`http://${process.env.PROFIL_HOST}:5006/profil/${userId}`)
+            const data = await res.json();
+            console.log(data);
+            return {
+                props: {
+                    username: data.username,
+                    email: data.email,
+                },
+            }
+
+        }
+
+    },[]);
+
+        return (
         <Container>
             <div>
                 <h1>Profil</h1>
+                <p>Username: {props.username}</p>
+                <p>Email: {props.email}</p>
             </div>
         </Container>
 
